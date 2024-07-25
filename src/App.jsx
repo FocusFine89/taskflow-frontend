8494c385b7e9ed9d750a9358bf8d98ac61ba7e66
FocusFine89 <niivanov02@gmail.com>
2024-07-25 10:45:09 +0200

    installazione react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
